c10886f0b4182ff9fc932c1aaed22bdd25731ec4
Erick Rick <erickcredjes@gmail.com>
2025-09-06 07:28:15 -0300

    nmp e node_module

---
+++ 01-modulos-no-nodejs/04-modulos-npm/index.js
@@ -0,0 +1,6 @@
+const uuid = require("uuid");
+
+console.log(`ID Gerado: ${uuid.v1()}`);
+console.log(`ID Gerado: ${uuid.v4()}`);
+console.log(`ID Gerado: ${uuid.v6()}`);
+console.log(`ID Gerado: ${uuid.v7()}`);
